b259fda15a24e5e0caa0d6deb6679fbb1a42f312
Aleksey Denisov <alde.owl@gmail.com>
2019-01-09 17:02:48 +0700

    -

--- src/scripts/index.js
+++
@@ -1,7 +0,0 @@
-let button = document.querySelector('.down__btn');
-let target = document.querySelector('.second');
-
-button.addEventListener('click', function() {
-  target.scrollIntoView({behavior: 'smooth'});
-});
-
